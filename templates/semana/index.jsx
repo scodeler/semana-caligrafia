959f23d0aed9b62d6c7b0039905b93c531400e2a
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-06-29 17:35:12 -0300

    feat: add title tag

--- templates/semana/index.jsx
+++ templates/semana/index.jsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import Head from 'next/head'
 
 const Semana = () => {
   return (
     <>
+      <Head>
+        <title>Semana da Caligrafia</title>
+      </Head>
       <h1 className="logo">
         <Image src="/logo.png" width="430" height="200" layout="responsive" />
       </h1>
