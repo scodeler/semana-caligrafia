50ae1e310d030848ebcc23d5a706465c7008d5ba
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-06-29 22:15:17 -0300

    feat: add profile section html

---
+++ templates/semana/parts/Profile.jsx
@@ -0,0 +1,27 @@
+const Profile = () => {
+  return (
+    <section className='profile'>
+      <div className='profile-box'>
+        <h2 className='profile-title'>
+          Quem está convidado para esta jornada?
+        </h2>
+        <ul className='profile-list'>
+          <li className='profileTop'>
+            Quem sonha em um dia entrar no mundo das letras
+          </li>
+          <li className='profileTop'>
+            Quem está no mundo das letras mas não consegue evoluir
+          </li>
+          <li className='profileTop'>
+            Quem busca um momento de qualidade pra si
+          </li>
+          <li className='profileTop'>
+            Quem busca se conectar melhor consigo mesmo
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default Profile
