57a5b2869d38734d60b94ad6f9e13bbec5d4cce7
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-06-30 14:35:37 -0300

    feat: video styling

--- templates/semana/parts/Video.jsx
+++ templates/semana/parts/Video.jsx
@@ -1,17 +1,19 @@
 const VideoEmbed = () => {
   return (
     <section className='video'>
-      <div className='video-container'>
-        <div className='videoWrapper'>
-          <iframe
-            width='560'
-            height='315'
-            src='https://www.youtube.com/embed/YVQqEoSXV8Q'
-            title='YouTube video player'
-            frameBorder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-            allowFullScreen
-          ></iframe>
+      <div className='container'>
+        <div className='video-container'>
+          <div className='videoWrapper'>
+            <iframe
+              width='560'
+              height='315'
+              src='https://www.youtube.com/embed/YVQqEoSXV8Q'
+              title='YouTube video player'
+              frameBorder='0'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
       </div>
     </section>
