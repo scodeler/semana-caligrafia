76189c35d8cbfd5a00657fd3c2927509b3f412fb
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-06-30 13:30:45 -0300

    feat: hero styling

--- templates/semana/parts/Hero.jsx
+++ templates/semana/parts/Hero.jsx
@@ -5,46 +5,53 @@ import SubscribeForm from './SubscribeForm'
 const Hero = () => {
   return (
     <section className='hero'>
-      <div className='hero-left'>
-        <h1 className='logo'>
-          <Image src='/logo.png' width='430' height='200' layout='responsive' />
-        </h1>
-        <h2 className='hero-title'>
-          Você vai transformar sua letra em arte em apenas 7 dias
-        </h2>
-        <div className='hero-text'>
-          <p>
-            Vou te mostrar tudo o que eu fiz e que me ajudou a chegar onde eu
-            cheguei, para você deixar se ser só um espectador e{' '}
-            <strong className='red'>entrar no mundo mágico das letras</strong>{' '}
-            de uma vez.
-          </p>
+      <div className='container'>
+        <div className='hero-left'>
+          <h1 className='logo'>
+            <Image
+              src='/logo.png'
+              width='430'
+              height='200'
+              layout='responsive'
+            />
+          </h1>
+          <h2 className='hero-title'>
+            Você vai transformar sua letra em arte em apenas 7 dias
+          </h2>
+          <div className='hero-text'>
+            <p>
+              Vou te mostrar tudo o que eu fiz e que me ajudou a chegar onde eu
+              cheguei, para você deixar se ser só um espectador e{' '}
+              <strong className='red'>entrar no mundo mágico das letras</strong>{' '}
+              de uma vez.
+            </p>
+          </div>
+          <ul className='hero-topics'>
+            <li className='hero-topic'>
+              <FiCalendar className='hero-topicIcon' />
+              19 a 24 de julho
+            </li>
+            <li className='hero-topic'>
+              <FiPenTool className='hero-topicIcon' />
+              Conteúdo Prático
+            </li>
+            <li className='hero-topic'>
+              <FiWifi className='hero-topicIcon' />
+              Online e gratuito
+            </li>
+            <li className='hero-topic'>
+              <FiUsers className='hero-topicIcon' />
+              Comunidade Exclusiva
+            </li>
+          </ul>
         </div>
-        <ul className='hero-topics'>
-          <li className='hero-topic'>
-            <FiCalendar className='hero-topicIcon' />
-            19 a 24 de julho
-          </li>
-          <li className='hero-topic'>
-            <FiPenTool />
-            Conteúdo Prático
-          </li>
-          <li className='hero-topic'>
-            <FiWifi />
-            Online e gratuito
-          </li>
-          <li className='hero-topic'>
-            <FiUsers />
-            Comunidade Exclusiva
-          </li>
-        </ul>
-      </div>
-      <div className='hero-form'>
-        <figure className='hero-AuthorImage'>
-          <Image src='/reginato.jpg' width='550' height='295' />
-        </figure>
-        <div className='hero-formContent'>
-          <SubscribeForm />
+        <div className='hero-form'>
+          <figure className='hero-AuthorImage'>
+            <Image src='/reginato.jpg' width='550' height='295' />
+          </figure>
+          <div className='hero-formContent'>
+            <SubscribeForm />
+          </div>
         </div>
       </div>
     </section>
