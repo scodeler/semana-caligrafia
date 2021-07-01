7da470bb25fc81ff10261bf0e871b7a16de3bb49
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-07-01 09:37:56 -0300

    feat: add form validation

--- templates/semana/parts/SubscribeForm.jsx
+++ templates/semana/parts/SubscribeForm.jsx
@@ -1,19 +1,53 @@
 import TextInput from '../../../components/TextInput'
 import { FiLock, FiAlertCircle } from 'react-icons/fi'
 import { HiCheckCircle } from 'react-icons/hi'
+import { useState } from 'react'
+import * as yup from 'yup'
 
 const SubscribeForm = () => {
+  const [error, setError] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+
+  const schema = yup.object().shape({
+    name: yup.string(3).required(),
+    email: yup.string().email().required(),
+  })
+
+  const handleName = (event) => {
+    setName(event.target.value)
+  }
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault()
+    const formIsValid = await schema.isValid({ name, email })
+    if (formIsValid) {
+      console.log('sucess')
+      setError(false)
+    } else {
+      setError(true)
+    }
+  }
+
   return (
-    <form className='subscribeForm'>
+    <form className='subscribeForm' onSubmit={handleFormSubmit}>
       <h3 className='subscribeForm-title'>Inscrição Gratuita</h3>
       <TextInput
         label='Nome'
         name='userName'
+        onChange={handleName}
+        value={name}
         placeholder='Como quer ser chamada(o)?'
       />
       <TextInput
         label='Email'
         name='userEmail'
+        value={email}
+        onChange={handleEmail}
         placeholder='Qual o seu melhor email?'
         type='email'
       />
@@ -21,6 +55,9 @@ const SubscribeForm = () => {
         <HiCheckCircle className='subscribeForm-alert' />
         Ao enviar este formulário você concorda em receber nossas comunicações
       </span>
+      {error && (
+        <p className='subscribeForm-error'>Preencha os dados corretamente</p>
+      )}
       <button type='submit' className='submitBtn green'>
         Garantir minha vaga
       </button>
