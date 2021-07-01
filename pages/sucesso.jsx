ada4cfeb15686e7dabbf641428e0edf0f5b56672
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-07-01 12:50:19 -0300

    feat: add particles

--- pages/sucesso.jsx
+++ pages/sucesso.jsx
@@ -3,6 +3,8 @@ import Head from 'next/head'
 import { ImWhatsapp, ImFacebook2 } from 'react-icons/im'
 import { RiInstagramLine } from 'react-icons/ri'
 import { FaTelegram, FaYoutube } from 'react-icons/fa'
+import Particles from 'react-tsparticles'
+import Footer from '../templates/semana/parts/Footer'
 
 const SuccessPage = () => {
   return (
@@ -80,7 +82,113 @@ const SuccessPage = () => {
             </div>
           </li>
         </ul>
+        <Footer />
       </div>
+      <Particles
+        id='tsparticles'
+        options={{
+          background: {
+            color: {
+              value: '#1D2536',
+            },
+            position: '50% 50%',
+            repeat: 'no-repeat',
+            size: '20%',
+          },
+          fullScreen: {
+            enable: true,
+            zIndex: -1,
+          },
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'repulse',
+              },
+              onHover: {
+                enable: true,
+                mode: 'bubble',
+              },
+            },
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                opacity: 0,
+                size: 0,
+              },
+              grab: {
+                distance: 400,
+              },
+              repulse: {
+                distance: 400,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: '#ffffff',
+            },
+            links: {
+              color: {
+                value: '#ffffff',
+              },
+              distance: 150,
+              opacity: 0.4,
+            },
+            move: {
+              attract: {
+                rotate: {
+                  x: 600,
+                  y: 600,
+                },
+              },
+              enable: true,
+              path: {},
+              outModes: {
+                bottom: 'out',
+                left: 'out',
+                right: 'out',
+                top: 'out',
+              },
+              random: true,
+              speed: 1,
+            },
+            number: {
+              density: {
+                enable: true,
+              },
+              value: 160,
+            },
+            opacity: {
+              random: {
+                enable: true,
+              },
+              value: {
+                min: 0,
+                max: 1,
+              },
+              animation: {
+                enable: true,
+                speed: 1,
+              },
+            },
+            size: {
+              random: {
+                enable: true,
+              },
+              value: {
+                min: 1,
+                max: 3,
+              },
+              animation: {
+                speed: 4,
+                minimumValue: 0.3,
+              },
+            },
+          },
+        }}
+      />
     </section>
   )
 }
