366a7e4e717fd9dc6fc79f0db1299d9d88e09ac8
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-07-02 14:23:29 -0300

    feat: add gtm tag

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -6,12 +6,31 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TCG654J');`,
+          }}
+        />
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <link rel='stylesheet' href='https://use.typekit.net/fsm0zgf.css' />
         <link rel='icon' href='/favicon.png' />
       </Head>
-      <Component {...pageProps} />
+      <body>
+        <noscript>
+          <iframe
+            src='https://www.googletagmanager.com/ns.html?id=GTM-TCG654J'
+            height='0'
+            width='0'
+            style='display:none;visibility:hidden'
+          ></iframe>
+        </noscript>
+        <Component {...pageProps} />
+      </body>
     </>
   )
 }
