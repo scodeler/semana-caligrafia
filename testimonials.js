788c07b0b61831a2675eb28d62610421b154b547
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2021-07-18 16:47:12 -0300

    feat: add testimonials

---
+++ testimonials.js
@@ -0,0 +1,67 @@
+const testimonials = [
+  {
+    id: 0,
+    videoId: '8ue0Ri7LTfU',
+    title: 'Ivonete, Cianorte-PR',
+    excerpt:
+      'Eu gosto tanto, que é de sábado, domingo. Todos os dias da semana, é de dia, de noite, tô escrevendo',
+  },
+  {
+    id: 1,
+    videoId: 'U7cHb02n8Sw',
+    title: 'Rafael, S. Bernardo-SP',
+    excerpt:
+      'A caligrafia me trouxe um gosto de viver que eu estava perdendo, isso me revigorou, me deu um gás.',
+  },
+  {
+    id: 2,
+    videoId: 'BU0MKq19XGs',
+    title: 'Carol, Piracicaba-SP',
+    excerpt:
+      'Eu escrevi cartas para a minha sobrinha que eu não via desde o Natal.',
+  },
+  {
+    id: 3,
+    videoId: '10eYW9R1Z-o',
+    title: 'Ir. Maria Angélica, Palmas-TO',
+    excerpt:
+      'Às vezes eu não consigo nem dormir de tantos pedidos que vem. Eu me sinto realizada na Escola de Caligrafia.',
+  },
+  {
+    id: 4,
+    videoId: 'qFUgKy0Ppbc',
+    title: 'Arthur, Rio de Janeiro-RJ',
+    excerpt:
+      'Eu aproveitava os aniversários e escrevia Parabéns pros amigos usando a caligrafia.',
+  },
+  {
+    id: 5,
+    videoId: 'F8vEPuRvNzI',
+    title: 'Thayná, São Paulo/SP',
+    excerpt:
+      'Eu tinha uma rotina no trabalho muito pesada. A caligrafia era um momento muito prazeroso que eu usava para relaxar.',
+  },
+  {
+    id: 6,
+    videoId: 'fwuT-F8PNbM',
+    title: 'Renato, Nova Friburgo-RJ',
+    excerpt:
+      'Uma coisa que me dá muito prazer é saber que a pessoa tem um item de decoração feito por mim na casa dela.',
+  },
+  {
+    id: 7,
+    videoId: 'mv887QV1DsI',
+    title: 'Bruna, Porto Alegre-RS',
+    excerpt:
+      'Hoje em dia eu confesso que gosto mais da caligrafia que do lettering',
+  },
+  {
+    id: 8,
+    videoId: 'bFf40mQvVI4',
+    title: 'Raíssa, São Paulo/SP',
+    excerpt:
+      'A caligrafia é como se fosse uma meditação. Você vê na sua própria letra que está se acalmando.',
+  },
+]
+
+export default testimonials
