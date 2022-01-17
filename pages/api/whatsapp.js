dd13dcb665e2ac985fde8e398ff0088e8d0c4b6f
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2022-01-17 10:37:14 -0300

    fix: test error log

--- pages/api/whatsapp.js
+++ pages/api/whatsapp.js
@@ -1,24 +1,28 @@
 import { connectToDatabase } from '../../util/mongodb'
 export default async (req, res) => {
-  const { db } = await connectToDatabase()
-  const groupData = await db.collection('whatsapp').find({}).toArray()
-  if (groupData[0].users > 300) {
-    const updatedUserCount = 0
-    await db
-      .collection('whatsapp')
-      .updateOne(
-        { _id: groupData[0]._id },
-        { $set: { group: groupData[0].group + 1, users: updatedUserCount } }
-      )
-  } else {
-    const updatedUserCount = groupData[0].users + 1
-    await db
-      .collection('whatsapp')
-      .updateOne(
-        { _id: groupData[0]._id },
-        { $set: { users: updatedUserCount } }
-      )
+  try {
+    const { db } = await connectToDatabase()
+    const groupData = await db.collection('whatsapp').find({}).toArray()
+    if (groupData[0].users > 300) {
+      const updatedUserCount = 0
+      await db
+        .collection('whatsapp')
+        .updateOne(
+          { _id: groupData[0]._id },
+          { $set: { group: groupData[0].group + 1, users: updatedUserCount } }
+        )
+    } else {
+      const updatedUserCount = groupData[0].users + 1
+      await db
+        .collection('whatsapp')
+        .updateOne(
+          { _id: groupData[0]._id },
+          { $set: { users: updatedUserCount } }
+        )
+    }
+    const updatedGroupData = await db.collection('whatsapp').find({}).toArray()
+    res.json(updatedGroupData[0])
+  } catch (err) {
+    console.log(err)
   }
-  const updatedGroupData = await db.collection('whatsapp').find({}).toArray()
-  res.json(updatedGroupData[0])
 }
