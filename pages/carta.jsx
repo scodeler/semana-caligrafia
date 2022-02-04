41c180477514809a99ff6338b12816f9d2eda3bf
Bruno Scodeler <bruno@estudiomaquinario.com.br>
2022-02-04 10:47:30 -0300

    feat: add tsunami effect

---
+++ pages/carta.jsx
@@ -0,0 +1,230 @@
+import Image from 'next/image'
+
+const Carta = () => {
+  return (
+    <section className='carta'>
+      <div
+        className='container'
+        style={{ margin: '80px auto 50px', padding: '0 30px', maxWidth: 800 }}
+      >
+        <h2>Carta aos participantes da Semana da Caligrafia</h2>
+
+        <p>
+          Estou realmente muito feliz por mais esta Semana da Caligrafia.
+          Sinceramente, n&atilde;o imaginava que ter&iacute;amos mais de{' '}
+          <strong>13 mil pessoas participando</strong>, tanta gente
+          compartilhando trabalhos e hist&oacute;rias na Comunidade.
+        </p>
+
+        <p>
+          Espero realmente que esta semente da caligrafia possa germinar e dar
+          frutos em voc&ecirc; e em cada um dos milhares de participantes.
+        </p>
+
+        <p>
+          Agora, eu tenho&nbsp; 2 not&iacute;cias pra voc&ecirc; : uma boa e
+          outra n&atilde;o muito boa.
+        </p>
+        <br />
+        <p dir='ltr'>
+          <strong>PRIMEIRO, A BOA NOT&Iacute;CIA&nbsp; </strong>😀
+        </p>
+
+        <p dir='ltr'>
+          Voc&ecirc; j&aacute; deve estar sabendo que segunda-feira eu vou ABRIR
+          INSCRI&Ccedil;&Otilde;ES pra &uacute;ltima turma da ESCOLA DE
+          CALIGRAFIA de 2021.
+        </p>
+
+        <p dir='ltr'>
+          Na Escola de Caligrafia eu ofere&ccedil;o um curso 100% online onde
+          ensino pessoas comuns a se realizarem e encontrarem um ref&uacute;gio
+          na rotina do dia a dia atrav&eacute;s das letras, mesmo
+          come&ccedil;ando a praticar agora.
+        </p>
+        <br />
+
+        <h3 dir='ltr'>
+          Veja aqui os&nbsp; B&Ocirc;NUS&nbsp; voc&ecirc; recebe ao se inscrever
+          na nova turma:
+        </h3>
+
+        <p dir='ltr'>
+          ✅ Entrada pro FESTIVAL DAS LETRAS: um evento de Caligrafia exclusivo
+          para alunos
+        </p>
+
+        <p dir='ltr'>✅ Acesso &agrave; COMUNIDADE FECHADA dos Calixonados.</p>
+
+        <p dir='ltr'>
+          ✅ MATERIAIS exclusivos, te&oacute;ricos e pr&aacute;ticos, incluindo
+          PAUTAS dos alfabetos.
+        </p>
+
+        <p dir='ltr'>
+          Tudo isso preparado com muito carinho pra receber VOC&Ecirc; entre os
+          nossos novos alunos.
+        </p>
+
+        <br />
+
+        <h3 dir='ltr'>
+          Mas n&atilde;o para por a&iacute;. Olha s&oacute; o que mais
+          voc&ecirc; vai receber:
+        </h3>
+
+        <p dir='ltr'>⭐&nbsp; 50% DE DESCONTO</p>
+
+        <p dir='ltr'>
+          ⭐ &nbsp;ATENDIMENTO INDIVIDUAL + KIT EXCLUSIVO &nbsp;para os 20
+          primeiros inscritos.
+        </p>
+
+        <p>
+          <br />
+        </p>
+
+        <p dir='ltr'>
+          E olha s&oacute; o KIT EXCLUSIVO que os 25 primeiros v&atilde;o
+          receber 👇
+        </p>
+
+        <Image width={800} height={533} src='/kit-exclusivo.jpg' />
+
+        <p dir='ltr'>
+          <em>
+            (Imagem do Kit exclusivo para os 25 primeiros inscritos contendo 1
+            caderno de pauta, 1 sketchbook, 1 caneta Brush Pen, 1 caneta
+            esferogr&aacute;fica, adesivos personalizados.)
+          </em>
+        </p>
+
+        <br />
+        <p dir='ltr'>
+          As inscri&ccedil;&otilde;es v&atilde;o abrir &agrave;s&nbsp; 08H DA
+          MANH&Atilde; &nbsp;⏰ (hor&aacute;rio de Bras&iacute;lia). Seja
+          r&aacute;pida se quiser garantir o ATENDIMENTO INDIVIDUAL comigo e o
+          KIT EXCLUSIVO.
+        </p>
+
+        <h3 dir='ltr'>AGORA, A M&Aacute; NOT&Iacute;CIA 😪</h3>
+
+        <p dir='ltr'>
+          Como eu disse, foram &nbsp;mais de 13 mil inscritos nesse evento .
+          Como esta ser&aacute; a &uacute;ltima turma do ano eu acredito que
+          teremos uma grande quantidade de pessoas interessadas.
+        </p>
+
+        <p dir='ltr'>
+          Ent&atilde;o vamos precisar limitar a quantidade de vagas.
+          Naturalmente, j&aacute; que queremos cuidar do bom atendimento aos
+          alunos.
+        </p>
+
+        <Image width={739 * 0.45} height={1600 * 0.45} src='/leads-ac.png' />
+
+        <p dir='ltr'>
+          <em>
+            (Print da tela do gerenciador de e-mail no per&iacute;odo de
+            inscri&ccedil;&otilde;es para a Semana da Caligrafia)
+          </em>
+        </p>
+
+        <br />
+
+        <p dir='ltr'>
+          Eu sei que pode ser chato mas &eacute; importante pra gente.
+        </p>
+
+        <p dir='ltr'>
+          Ao mesmo tempo que queremos ter muitos alunos na Escola, precisamos
+          cuidar da qualidade do aprendizado. Ent&atilde;o preferimos garantir
+          essa aten&ccedil;&atilde;o mais cuidadosa.
+        </p>
+
+        <p dir='ltr'>
+          No pr&oacute;ximo ano, prometemos investir para melhorar nossa
+          capacidade de atendimento. 😉
+        </p>
+
+        <br />
+
+        <h3 dir='ltr'>PARA SE INSCREVER &eacute; f&aacute;cil 👇</h3>
+
+        <p dir='ltr'>
+          1️⃣&nbsp; Segunda-feira, dia 06, &agrave;s 08h da manh&atilde; ,
+          voc&ecirc; vai receber no email o link da p&aacute;gina de
+          inscri&ccedil;&atilde;o da ESCOLA DE CALIGRAFIA. L&aacute;, voc&ecirc;
+          vai encontrar todas as informa&ccedil;&otilde;es sobre o curso: quais
+          s&atilde;o os m&oacute;dulos, como vai funcionar, o pre&ccedil;o,
+          formas de pagamento, garantia...
+        </p>
+
+        <p dir='ltr'>
+          2️⃣ &nbsp;Com certeza, as primeiras 20 vagas &nbsp;(que v&atilde;o
+          receber o KIT DE CALIGRAFIA + ATENDIMENTO INDIVIDUAL) devem se esgotar
+          em alguns minutos. Pensando nisso, costumamos criar um Grupo Vip no
+          WhatsApp pros mais interessados.
+        </p>
+
+        <p dir='ltr'>
+          Quem estiver nesse grupo vai receber o link de inscri&ccedil;&atilde;o
+          &agrave;s 7h antes. Ou seja, poder&aacute; fazer as
+          inscri&ccedil;&otilde;es 1h antes da maioria das pessoas.
+        </p>
+
+        <p dir='ltr'>
+          Sem d&uacute;vida, Isso vai aumentar sua chance de conseguir uma vaga
+          entre os primeiros e ganhar o ATENDIMENTO INDIVIDUAL e o KIT
+          EXCLUSIVO.
+        </p>
+
+        <p dir='ltr'>
+          3️⃣&nbsp; Para entrar do Grupo Vip &nbsp;e ter a chance de estar entre
+          os primeiros, &eacute; s&oacute; clicar abaixo:
+        </p>
+
+        <p dir='ltr'>
+          <a href='http://semanadacaligrafia.com.br/whatsapp-vip'>
+            &lt; QUERO ENTRAR NO GRUPO VIP &gt;
+          </a>
+        </p>
+
+        <br />
+
+        <h3>FICOU ALGUMA D&Uacute;VIDA?</h3>
+
+        <p dir='ltr'>
+          Voc&ecirc; pode envi&aacute;-la respondendo esse e-mail ou
+          ent&atilde;o l&aacute; na Comunidade da Semana da Caligrafia. Vou
+          responder o mais r&aacute;pido poss&iacute;vel.
+        </p>
+
+        <p dir='ltr'>Espero te ver nessa nova turma 😄</p>
+
+        <p dir='ltr'>
+          E se voc&ecirc; quiser rever as aulas da Semana da Caligrafia, ainda
+          d&aacute; tempo. As grava&ccedil;&otilde;es ficar&atilde;o
+          dispon&iacute;veis at&eacute; este domingo, &agrave;s 23h59. &Eacute;
+          s&oacute; clicar nesse link abaixo:
+        </p>
+
+        <p dir='ltr'>
+          <a href='https://www.semanadacaligrafia.com.br/aulas/1'>
+            &lt; REVER AS AULAS DA SEMANA DA CALIGRAFIA &gt;
+          </a>
+        </p>
+
+        <p>
+          <br />
+        </p>
+
+        <p dir='ltr'>Almacalma e boas letras.</p>
+
+        <p dir='ltr'>Thiago Reginato (@tipocali)</p>
+      </div>
+    </section>
+  )
+}
+
+export default Carta
